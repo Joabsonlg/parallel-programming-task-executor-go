6d9f26fea9234be7f04d60d033b4dd110756ecdd
joabson <joabsonlg918@gmail.com>
2023-11-12 16:32:20 -0300

    Adiciona documentação

--- src/main/main.go
+++ src/main/main.go
@@ -10,6 +10,7 @@ import (
 	"time"
 )
 
+// TaskType Declaração de um tipo personalizado para diferenciar tarefas de leitura e escrita
 type TaskType int
 
 const (
@@ -17,6 +18,7 @@ const (
 	WRITING
 )
 
+// Task Estrutura para armazenar detalhes de uma tarefa
 type Task struct {
 	ID       int
 	Custo    float64
@@ -24,17 +26,20 @@ type Task struct {
 	Value    int
 }
 
+// Result Estrutura para armazenar o resultado de uma tarefa processada
 type Result struct {
 	ID     int
 	Result string
 	Time   int64
 }
 
+// Executor Estrutura para gerenciar filas de tarefas e resultados
 type Executor struct {
 	TaskQueue chan Task
 	Results   chan Result
 }
 
+// NewExecutor Função para criar um novo Executor com tamanhos de fila especificados
 func NewExecutor(taskQueueSize, resultQueueSize int) *Executor {
 	return &Executor{
 		TaskQueue: make(chan Task, taskQueueSize),
@@ -42,10 +47,12 @@ func NewExecutor(taskQueueSize, resultQueueSize int) *Executor {
 	}
 }
 
-func worker(tasks chan Task, sharedFile *os.File, results chan Result, rwMutex *sync.RWMutex, wg *sync.WaitGroup) {
-	defer wg.Done()
+// Função worker para processar tarefas. Cada worker executa em uma goroutine separada.
+func worker(workerID int, tasks chan Task, sharedFile *os.File, results chan Result, rwMutex *sync.RWMutex, wg *sync.WaitGroup) {
+	defer wg.Done() // Sinaliza a conclusão da goroutine ao WaitGroup ao retornar
 	for task := range tasks {
 		startTime := time.Now()
+		//fmt.Printf("Worker %d está processando a tarefa %d\n", workerID, task.ID)
 		if task.TaskType == WRITING {
 			write(task, sharedFile, rwMutex)
 		} else {
@@ -53,55 +60,57 @@ func worker(tasks chan Task, sharedFile *os.File, results chan Result, rwMutex *
 		}
 		endTime := time.Now()
 		executionTime := endTime.Sub(startTime).Nanoseconds()
-		results <- Result{ID: task.ID, Time: executionTime}
+		results <- Result{ID: task.ID, Time: executionTime} // Envia o resultado para o canal Results
 	}
 }
 
+// Função write para processar tarefas de escrita
 func write(task Task, sharedFile *os.File, rwMutex *sync.RWMutex) {
 	time.Sleep(time.Duration(task.Custo*1000) * time.Millisecond)
 
-	rwMutex.Lock()
-	defer rwMutex.Unlock()
+	rwMutex.Lock()         // Bloqueia para escrita
+	defer rwMutex.Unlock() // Desbloqueia após a conclusão da função
 
-	_, err := sharedFile.Seek(0, 0)
+	_, err := sharedFile.Seek(0, 0) // Posiciona o ponteiro do arquivo no início
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
 	var existingValue int
-	_, err = fmt.Fscanln(sharedFile, &existingValue)
+	_, err = fmt.Fscanln(sharedFile, &existingValue) // Lê o valor existente no arquivo
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
 	newValue := existingValue + task.Value
-	sharedFile.Truncate(0)
-	sharedFile.Seek(0, 0)
-	_, err = sharedFile.WriteString(fmt.Sprintf("%d\n", newValue))
+	sharedFile.Truncate(0)                                         // Limpa o conteúdo do arquivo
+	sharedFile.Seek(0, 0)                                          // Reposiciona o ponteiro no início
+	_, err = sharedFile.WriteString(fmt.Sprintf("%d\n", newValue)) // Escreve o novo valor
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
-	fmt.Printf("W - Task %d : %d\n", task.ID, newValue)
+	//fmt.Printf("W - Task %d : %d\n", task.ID, newValue)
 }
 
+// Função read para processar tarefas de leitura
 func read(task Task, sharedFile *os.File, rwMutex *sync.RWMutex) {
 	time.Sleep(time.Duration(task.Custo*1000) * time.Millisecond)
 
-	rwMutex.RLock()
-	defer rwMutex.RUnlock()
+	rwMutex.RLock()         // Bloqueia para leitura
+	defer rwMutex.RUnlock() // Desbloqueia após a conclusão da função
 
-	_, err := sharedFile.Seek(0, 0)
+	_, err := sharedFile.Seek(0, 0) // Posiciona o ponteiro do arquivo no início
 	if err != nil {
 		log.Println(err)
 		return
 	}
 
 	var value string
-	_, err = fmt.Fscanln(sharedFile, &value)
+	_, err = fmt.Fscanln(sharedFile, &value) // Lê o valor do arquivo
 	if err != nil {
 		log.Println(err)
 		return
@@ -110,6 +119,7 @@ func read(task Task, sharedFile *os.File, rwMutex *sync.RWMutex) {
 	fmt.Printf("R - Task %d : %s\n", task.ID, value)
 }
 
+// Função main: ponto de entrada do programa
 func main() {
 	var nTasksAmount, tWorkersThreads, eWritingTasks int
 	fmt.Print("Digite o valor de N: ")
@@ -129,14 +139,14 @@ func main() {
 
 	// Gerar tarefas de forma misturada
 	taskId := 0
-	tasks := make([]Task, 0, tasksAmount)
+	tasks := make([]Task, 0, tasksAmount) // Cria um slice de tarefas vazio com tamanho inicial 0 e capacidade tasksAmount
 
-	for i := 0; i < writingTasks; i++ {
+	for i := 0; i < writingTasks; i++ { // Adiciona tarefas de escrita
 		tasks = append(tasks, Task{ID: taskId, Custo: rand.Float64() * 0.01, TaskType: WRITING, Value: rand.Intn(11)})
 		taskId++
 	}
 
-	for i := 0; i < readingTasks; i++ {
+	for i := 0; i < readingTasks; i++ { // Adiciona tarefas de leitura
 		tasks = append(tasks, Task{ID: taskId, Custo: rand.Float64() * 0.01, TaskType: READING, Value: rand.Intn(11)})
 		taskId++
 	}
@@ -166,17 +176,26 @@ func main() {
 	}
 	sharedFile.Sync()
 
+	startTime := time.Now()
+
 	var wg sync.WaitGroup
 	rwMutex := &sync.RWMutex{}
 
 	for i := 0; i < tWorkersThreads; i++ {
 		wg.Add(1)
-		go worker(executor.TaskQueue, sharedFile, executor.Results, rwMutex, &wg)
+		go worker(i, executor.TaskQueue, sharedFile, executor.Results, rwMutex, &wg)
 	}
 
 	go func() {
 		wg.Wait()
 		close(executor.Results) // Fechando Results após todas as goroutines terminarem
+
+		// Registrar o fim do processamento
+		endTime := time.Now()
+
+		// Calcular e exibir o tempo total
+		totalTime := endTime.Sub(startTime)
+		fmt.Printf("Tempo total de processamento: %s\n", totalTime)
 	}()
 
 	// Coletar resultados
